8e25826593fae173f6dbcefa99869534af842048
YingLuoNou <YingLuoNou@users.noreply.github.com>
2026-07-06 16:34:19 +0800

    fix ui2

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,10 @@
       </aside>
 
       <main class="relative z-10 flex-1 p-8 overflow-hidden">
-        <div class="h-full w-full rounded-[40px] glass-panel-light relative overflow-hidden">
+        <div
+          class="h-full w-full rounded-[40px] relative overflow-hidden"
+          :class="route.path === '/activity' ? 'bg-transparent border border-white/10' : 'glass-panel-light'"
+        >
           <router-view v-slot="{ Component }">
             <transition name="fade" mode="out-in">
               <component :is="Component" :key="route.path" />
